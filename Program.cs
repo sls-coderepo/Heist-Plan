d3c5358623822363ccc3db78a061ad28beb61f2a
Shirish Shrestha <shrestha_sl@hotmail.com>
2019-11-16 11:52:12 -0600

    Completed phase 2 exercise

--- Program.cs
+++ Program.cs
@@ -15,18 +15,39 @@ namespace Heist_Plan
             int skillLevel;
             decimal courageFactor;
 
-            Console.WriteLine("What is your name?");
-            name = Console.ReadLine();
+            bool startAgain = true;
+            string reply;
 
-            Console.WriteLine("What is your skill level?");
-            skillLevel = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("What is your Courage Factor?");
-            courageFactor = Convert.ToDecimal(Console.ReadLine());
+            while (startAgain)
+            {
+                Console.WriteLine("What is your name?");
+                name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+                Console.WriteLine("What is your skill level?");
+                skillLevel = Convert.ToInt32(Console.ReadLine());
 
-            members.Add(name, new Dictionary<string, decimal>()
-            { { "skillLevel", skillLevel }, { "courageFactor", courageFactor }
-            });
+                Console.WriteLine("What is your Courage Factor? (0.0 - 2.0)");
+                courageFactor = Convert.ToDecimal(Console.ReadLine());
+
+                members.Add(name, new Dictionary<string, decimal>()
+                { { "skillLevel", skillLevel }, { "courageFactor", courageFactor }
+                });
+
+                Console.Write("Add new member? (Y or N):  ");
+                reply = Console.ReadLine();
+                reply = reply.ToUpper();
+                if (reply != "Y")
+                {
+                    startAgain = false;
+                }
+
+            }
+
+            Console.WriteLine($"There are {members.Count} in your team!");
 
             foreach (KeyValuePair<string, Dictionary<string, decimal>> member in members)
             {
