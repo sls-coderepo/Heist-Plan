e2cc65d44c7bb7e95b2fa786cafd2f5e948a254f
Shirish Shrestha <shrestha_sl@hotmail.com>
2019-11-19 14:07:47 -0600

    Completed Heist Plan Exercise

--- Program.cs
+++ Program.cs
@@ -10,31 +10,51 @@ namespace Heist_Plan
             string message = "Plane Your Heist!";
             Console.WriteLine(message);
             Console.WriteLine("==============================================");
+            Console.Write("Enter difficulty level of the bank: ");
+            int bankDifficultyLevel = Convert.ToInt32(Console.ReadLine());
 
             List<Member> members = new List<Member>();
 
-
             bool startAgain = true;
             string reply;
-
-            int bankDifficultLevel = 100;
+            //int bankDifficultLevel = 100;
             decimal totalSkillLevel = 0;
             //Prompt for user input
             while (startAgain)
             {
-                Member member = new Member();
 
                 Console.WriteLine("What is your name?");
-                member.Name = Console.ReadLine();
-                if (member.Name == "")
+                string name = Console.ReadLine();
+                if (name == "")
                 {
                     break;
                 }
                 Console.WriteLine("What is your skill level?");
-                member.SkillLevel = Convert.ToInt32(Console.ReadLine());
+                string skillLevelString = Console.ReadLine();
+                int skillLevel;
+
+                try
+                {
+                    skillLevel = int.Parse(skillLevelString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"{skillLevelString} is not a valid skill level. Using a default value of 10");
+                    skillLevel = 10;
+                }
 
                 Console.WriteLine("What is your Courage Factor? (0.0 - 2.0)");
-                member.CourageFactor = Convert.ToDouble(Console.ReadLine());
+                string courageFactorString = Console.ReadLine();
+                decimal courageFactor;
+                try
+                {
+                    courageFactor = decimal.Parse(courageFactorString);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"{courageFactorString} is not a valid courage factor. Using a default value of 1.0");
+                    courageFactor = 1.0M;
+                }
 
                 Console.Write("Add new member? (Y or N):  ");
                 reply = Console.ReadLine();
@@ -44,11 +64,17 @@ namespace Heist_Plan
                     startAgain = false;
                 }
                 // adding member to the member list
+                Member member = new Member()
+                {
+                    Name = name,
+                    SkillLevel = skillLevel,
+                    CourageFactor = courageFactor
+                };
+
                 members.Add(member);
             }
             Console.WriteLine("==============================================");
             Console.WriteLine($"There are {members.Count} members in your team!");
-
 
             foreach (var member in members)
             {
@@ -64,23 +90,29 @@ namespace Heist_Plan
             }
             // Random Number 
             Random rand = new Random();
-            bankDifficultLevel += rand.Next(-10, 10);
 
 
-            Console.WriteLine($"Bank Difficulty Level-- {bankDifficultLevel}");
-            Console.WriteLine($"Total Skill Level-- {totalSkillLevel}");
-            if (totalSkillLevel >= bankDifficultLevel)
+
+            Console.Write("Enter number of trial runs: ");
+            int numberOfTrail = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < numberOfTrail; i++)
             {
-                Console.WriteLine("We have enough skill level to heist the bank!");
+
+                int currentBankDifficultLevel = bankDifficultyLevel;
+                currentBankDifficultLevel += rand.Next(-10, 10);
+
+                if (totalSkillLevel >= currentBankDifficultLevel)
+                {
+                    Console.WriteLine("We have enough skill level to heist the bank!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, We cant heist the bank!");
+                }
+                Console.WriteLine($"Bank Difficulty Level-- {currentBankDifficultLevel}");
+                Console.WriteLine($"Total Skill Level-- {totalSkillLevel}");
+
             }
-            else
-            {
-                Console.WriteLine("Sorry, We cant heist the bank!");
-            }
-
-
-
-
 
 
         }
